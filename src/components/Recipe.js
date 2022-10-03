4cd642ec81347c7dd1886ceae5f931865c7d1b4f
DaniK178 <daniellekyere@gmail.com>
2022-10-03 15:34:45 +0100

    added recipe method'

---
+++ src/components/Recipe.js
@@ -0,0 +1,41 @@
+
+const Recipe = ({name, ingredients, rating}) => {
+
+return (
+    <div
+    className="recipe">
+        <header> Recipe {name} </header>
+        <main>
+        <>
+            <section>
+                {
+                    cakes.map((cake, index) => {
+                        return(
+                            <div key={index}>
+                                <h3>{cake.cakeName}</h3>
+                                <ul>
+                                    {cake.ingredients.map((ingredient, index) => {
+                                        return(
+                                            <li key={index}>{ingredient}</li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
+                        )
+                    })
+                }
+            </section>
+            <section>
+                <h4>Average cake rating: </h4>
+                <p>{(cakes.reduce((total, cake) => total + cake.rating, 0) / cakes.length)}</p>
+            </section>
+        </>
+        </main>
+    </div>
+
+
+)
+
+}
+
+export default Recipe;
